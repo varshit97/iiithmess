1837d9873d3bcf5236b3eacee06166c3d966a7e1
Varshit <varshit97@gmail.com>
2016-05-09 20:11:58 +0530

    Add CSS

--- main.js
+++ main.js
@@ -207,9 +207,3 @@ function showMenu(messNumber){
     document.getElementById('Snacks').innerHTML = yukthaarMessMenu[today]["Snacks"];
   }
 }
-
-// '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
-$(window).on("load resize ", function() {
-  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-  $('.tbl-header').css({'padding-right':scrollWidth});
-}).resize();
