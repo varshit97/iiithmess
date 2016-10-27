63170bfb6a9b8ed77d534d58b3129d62716704b2
ByronGeorge <bgeorge978@gmail.com>
2016-10-27 19:01:56 -0400

    added north mess menu

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ weekday[6] = "Saturday";
 var today = weekday[d.getDay()];
 
 var yukthaarMessMenu = {};
-yukthaarMessMenu[weekday[1]] = {
 "Breakfast" : "Ragi\nIdly-Sambar\nKaram podi",
 
 "Lunch" : "Masoor Dal\nLauki\nKadi\nVegetable pickle",
@@ -218,6 +217,77 @@ kadambVegMess[weekday[6]] = {
 "Snacks" : "CHOLEY CHAT, LEMON JUICE"
 };
 
+var northMessMenu = {};
+northMessMenu[weekday[1]] = {
+"Breakfast" : "Poori\nAlu Sabji\nBread Pakoda\nCornflakes\nFruits\nBread Butter Jam Milk\nTea",
+
+"Lunch" : "Rajama\nChana Dal\nMethi Aalu\nWhite Rice\nTomato Rice\nRoti\nDahi\nButter Milk\nAalu Tikki\nSalad\nLimboo Pani",
+
+"Dinner" : "Shahi Paneer\nLauki Sabji\nTuver Dal\nTandori Roti\nDahi\nWhite Rice\nPudina Rice\nSweet",
+
+"Snacks" : "poha"
+};
+
+northMessMenu[weekday[2]] = {
+"Breakfast" : "Veg Cutlet\nVeg Macroni\nCornflakes,\nFruits,\nBread Butter Jam Milk,\nTea",
+
+"Lunch" : "Dal Makhani,\nAalu Mutter,\nTurai Sabji,\nWhite Rice,\nLemon Rice,\nRoti,\nDahi,\nButter Milk,\nSalad,\nLimbo Pani,\nAachar",
+
+"Dinner" : "Aalu Paratha,\nMix Veg Sabji,\nDahi,\nButter Milk,\nTuver Dal,\nWhite Rice,\nFried Rice,\nJalebi,\nGreen & Sweet Chatani",
+
+"Snacks" : "chole chaat"
+};
+
+northMessMenu[weekday[3]] = {
+"Breakfast" : "Paneer Masala Dosa,\nMayonise Sandwich,\nCornflakes,\nFruits,\nBread Butter Jam,\nMilk,\nTea",
+
+"Lunch" : "Baingan Masala,\nAalu Cabbage,\nVeg Biryani,\nWhite Rice,\nMasoor Dal,\nRoti,\nDahi,\nVeg Raita,\nSalad,\nAachar,\nPapad,\nFruit custard",
+
+"Dinner" : "Mutter Paneer,\nGobi-Aalu,\nMasoor Dal,\nWhite rice,\nJeera Rice,\nRumali Roti,\nRoti,\nDahi,\nButter Milk,\nSalad,\nAachar,\nPapad",
+
+"Snacks" : "mix pakoda"
+};
+
+northMessMenu[weekday[4]] = {
+"Breakfast" : "Dhokla,\nVada Pav,\nCornflakes,\nFruits,\nBread Butter Jam Milk,\nTea",
+
+"Lunch" : "Chole,\nAalu sabji Dry,\nMassor Dal,\nPuri,\nPalak Puri,\nWhite Rice,\nJeera Rice,\nDahi,\nButter Milk,\nMimbu Pani,\nSalad,\nPapad",
+
+"Dinner" : "Aalu Gobi,\nTuver Dal,\nManchurian,\nRoti,\nDahi,\nButter Milk,\nWhite Rice,\nTomato Rice,\nSweet Boondi,\nSalad,\nAachar",
+
+"Snacks" : "Samosa"
+};
+
+northMessMenu[weekday[5]] = {
+"Breakfast" : "Noodles,\nIdli Samabar Chutney,\nCornflakes,\nFruits,\nBread Butter Jam,\nMilk,\nTea",
+
+"Lunch" : "Bhindi Fry,\nDum Aalu,\nTuver Dal,\nWhite Rice,\nFried Rice,\nRoti,\nDahi,\nPujabi Lassi,\nDahi Vada,\nSalad,\nLimboo Pani",
+
+"Dinner" : "Palak Paneer,\nAalu Gobi,\nChana Dal,\nWhite Rice,\nRoti,\nDahi,\nPudina Rice,\nSalad,\nAachar",
+
+"Snacks" : "bhel puri"
+};
+
+northMessMenu[weekday[6]] = {
+"Breakfast" : "Gobi or Aalu Paratha,\nDahi,\nGreen Chutney,\nVeg Sandwich,\nCornflakes,\nFruits,\nBread Butter Jam Milk,\nTea",
+
+"Lunch" : "Soya bean(gravy),\nAaluGobi,\nTuver Dal,\nAalu fry,\nWhite Rice,\nDhaniya Rice,\nRoti,\nDahi,\nRaita,\nSalad,\nLimboo pani,\nAachar",
+
+"Dinner" : "Chole Batur,\nWhite Rice,\nPulav,\nGluab Jamun,\nDahi,\nButter Milk,\nSalad,\nAachar,\nPapad",
+
+"Snacks" : "pav bhaji"
+};
+
+northMessMenu[weekday[0]] = {
+"Breakfast" : "Samosa,\nTomato Sauce,\nPoha,\nCornflakes,\nFruits,\nBread Butter Jam Milk,\nTea",
+
+"Lunch" : "Aalu Palak,\nVeg. Kofta,\nBhindi,\nTuver Dal,\nWhite Rice,\nPudina Rice,\nRotim,\nDahi,\nBundi Raita,\nSalad,\nLimboo Pani,\nAachar,\nSooji Halwa",
+
+"Dinner" : "Aalu Shimala Mirch,\nPaneer Dry,\nTuver Dal,\nWhite Rice,\nLemon Rice,\nParotha,\nDahi,\nSalad",
+
+"Snacks" : "veg cutlet"
+};
+
 
 var messNum = null;
 
@@ -249,6 +319,15 @@ function showMenuByMess(messNumber){
     document.getElementById('Lunch').innerHTML = kadambVegMess[today]["Lunch"];
     document.getElementById('Dinner').innerHTML = kadambVegMess[today]["Dinner"];
     document.getElementById('Snacks').innerHTML = kadambVegMess[today]["Snacks"];
+  }
+  if(messNumber==4)
+  {
+    document.getElementById('messyMess').innerHTML = "North";
+    document.getElementById('Day').innerHTML = today;
+    document.getElementById('Breakfast').innerHTML = northMessMenu[today]["Breakfast"];
+    document.getElementById('Lunch').innerHTML = northMessMenu[today]["Lunch"];
+    document.getElementById('Dinner').innerHTML = northMessMenu[today]["Dinner"];
+    document.getElementById('Snacks').innerHTML = northMessMenu[today]["Snacks"];
   }
 }
 
@@ -293,6 +372,14 @@ function showMenuByDay(day){
       cell3.innerHTML = kadambVegMess[weekday[day]]["Lunch"];
       cell4.innerHTML = kadambVegMess[weekday[day]]["Dinner"];
       cell5.innerHTML = kadambVegMess[weekday[day]]["Snacks"];
+    }
+    if(messNum == 4)
+    {
+      cell1.innerHTML = weekday[day];
+      cell2.innerHTML = northMessMenu[weekday[day]]["Breakfast"];
+      cell3.innerHTML = northMessMenu[weekday[day]]["Lunch"];
+      cell4.innerHTML = northMessMenu[weekday[day]]["Dinner"];
+      cell5.innerHTML = northMessMenu[weekday[day]]["Snacks"];
     }
     inTable[day] = weekday[day];
   }
